742c49097839be01d571a1621ea3d4f1610fa203
sudalking94 <67311672+sudalking94@users.noreply.github.com>
2021-03-14 04:32:48 +0900

    updated

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import os
 import math
+import random
 from flask import Flask, flash, render_template, redirect, url_for, request
 
 
 app = Flask(__name__)
 
-sign = "@#$%"
+sign = "!@#$%^&*()"
 number = "0123456789"
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-def make_password(length):
+def make_password(length, args):
+    total_string = ""
+    for k in args:
+        if k == "sign":
+            total_string += sign
+        elif k == "number":
+            total_string += number
+        elif k == "lower_case":
+            total_string += lower_case
+        elif k == "upper_case":
+            total_string += upper_case
 
-    return pass
+    total_string_list = list(total_string)
+    random.shuffle(total_string_list)
+    total_string = ''.join(total_string_list)
+    if length == "10":
+        total_string = total_string[0:10]
+        return total_string
+    else:
+        if len(total_string) < 20:
+            temp = total_string[0:10]
+            total_string_list = list(total_string)
+            random.shuffle(total_string_list)
+            total_string = temp + ''.join(total_string_list[0:10])
+            total_string = ''.join(total_string)
+            return total_string
+        else:
+            total_string = total_string[0:20]
+            return total_string
 
 
 @app.route("/")
@@ -23,13 +50,12 @@ def home():
 
 @app.route("/<result>")
 def result(result):
+    total_string = ""
     if result == "result":
         length = request.args["length"]
-        if length == 10:
-            result = make_password(length)
-        else:
-            result = make_password(length)
-        return render_template(f"{result}.html", result=result)
+        args = request.args
+        total_string = make_password(length, args)
+        return render_template(f"{result}.html", result=total_string)
     else:
         return redirect(url_for("home"))
 
